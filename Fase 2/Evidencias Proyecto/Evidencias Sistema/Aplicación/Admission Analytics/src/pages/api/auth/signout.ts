fd057e26805bf78e65451b8edf26fdc47251f4c1
ANTONIO IGNACIO VEGA VEGA <106415007+antovegav@users.noreply.github.com>
2025-11-17 23:38:42 -0300

    Delete Fase 2/Evidencias Proyecto/Evidencias Sistema/Aplicación/Admission Analytics directory

--- Fase 2/Evidencias Proyecto/Evidencias Sistema/Aplicación/Admission Analytics/src/pages/api/auth/signout.ts
+++
@@ -1,8 +0,0 @@
-import type { APIRoute } from "astro";
-import { createServerSupabase } from "@/lib/supabase/supabaseServer"; 
-
-export const GET: APIRoute = async ({ request, cookies, redirect }) => {
-    const supabase = createServerSupabase({ request, cookies }); 
-    await supabase.auth.signOut();  // Limpiar sesión y cookies del servidor/navegador
-    return redirect("/auth/login"); // Redirigir
-};
